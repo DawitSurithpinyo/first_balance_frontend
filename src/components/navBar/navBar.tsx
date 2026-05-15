d741de8b3bb3e6a05566a7ca7ee4fb7e592e7653
Dawit <dawitsurithpinyo@gmail.com>
2026-05-15 15:40:55 +0700

    feat: init NavBar

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,11 @@
+import './navBar.css'
+
+export default function NavBar() {
+    return (
+        <div id="navbar">
+            <span>Dashboard</span>
+            <span>Entries</span>
+            <span>Account</span>
+        </div>
+    )
+}
