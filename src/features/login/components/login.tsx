17eb449ec2eca5e49842c35339805d1a0dd09299
Dawit <dawitsurithpinyo@gmail.com>
2026-05-21 17:00:28 +0700

    fix(Login): change redirection to Navigate component to prevent BrowserRouter state changing during render

--- src/features/login/components/login.tsx
+++ src/features/login/components/login.tsx
@@ -1,17 +1,16 @@
 import LoginWithGoogle from "./loginWithGoogle";
 import Banner from "./banner";
 import { useAuthContext } from "@/stores/authContext";
-import { useNavigate } from "react-router";
+import { Navigate } from "react-router-dom";
 
 export default function Login() {
     const { credentials, sessionIsChecked } = useAuthContext();
-    const navigate = useNavigate();
 
     // in-flight
     if (!sessionIsChecked) return <div>Loading...</div>;
 
     if (credentials) {
-        navigate("/dashboard");
+        return <Navigate to={"/dashboard"} />
     }
 
     return (
