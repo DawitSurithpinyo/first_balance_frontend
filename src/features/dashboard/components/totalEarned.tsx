0c9983e53cc0c81b8e178ec139368e7b43e8feb1
Dawit <dawitsurithpinyo@gmail.com>
2026-05-22 16:18:40 +0700

    feat(dashboard): implement barebone TotalEarned component

---
+++ src/features/dashboard/components/totalEarned.tsx
@@ -0,0 +1,22 @@
+import { type Transaction } from "@/types/transactions"
+import { useTransactions } from "@/stores/transactionsStore"
+
+export default function TotalEarned() {
+    const transactions = useTransactions((state) => state.transactions)
+    const getTotalEarned = (transactions: Transaction[]): number => {
+        var total: number = 0
+        for(let i = 0; i < transactions.length; i++) {
+            if(transactions[i].value > 0) {
+                total += transactions[i].value
+            }
+        }
+        return total
+    }
+    
+    return (
+        <>
+            <span>Total earned</span>
+            <span>{getTotalEarned(transactions).toString()}</span>
+        </>
+    )
+}
