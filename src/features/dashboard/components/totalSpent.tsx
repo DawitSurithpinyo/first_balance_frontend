ecb65b828a501f8ac18b7ef6a7e786b70fc7ed80
Dawit <dawitsurithpinyo@gmail.com>
2026-05-22 16:18:55 +0700

    feat(dashboard): implement barebone TotalSpent component

---
+++ src/features/dashboard/components/totalSpent.tsx
@@ -0,0 +1,22 @@
+import { type Transaction } from "@/types/transactions"
+import { useTransactions } from "@/stores/transactionsStore"
+
+export default function TotalSpent() {
+    const transactions = useTransactions((state) => state.transactions)
+    const getTotalSpent = (transactions: Transaction[]): number => {
+        var total: number = 0
+        for(let i = 0; i < transactions.length; i++) {
+            if(transactions[i].value < 0) {
+                total += transactions[i].value * -1
+            }
+        }
+        return total
+    }
+
+    return (
+        <>
+            <span>Total spent</span>
+            <span>{getTotalSpent(transactions).toString()}</span>
+        </>
+    )
+}
