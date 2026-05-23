f1980c0103f9776f73069de66730324b2556040a
Dawit <dawitsurithpinyo@gmail.com>
2026-05-23 12:00:45 +0700

    fix(eslint): use let instead of var; primitive number type

--- src/features/dashboard/components/totalSpent.tsx
+++ src/features/dashboard/components/totalSpent.tsx
@@ -4,7 +4,7 @@ import { useTransactions } from "@/stores/transactionsStore"
 export default function TotalSpent() {
     const transactions = useTransactions((state) => state.transactions)
     const getTotalSpent = (transactions: Transaction[]): number => {
-        var total: number = 0
+        let total: number = 0
         for(let i = 0; i < transactions.length; i++) {
             if(transactions[i].value < 0) {
                 total += transactions[i].value * -1
