bb2eb8cf6271a78b24423813203aedbd42e71a53
Dawit <dawitsurithpinyo@gmail.com>
2026-05-23 11:53:46 +0700

    feat(entries): add transactions fetching API in main Entries component

--- src/features/entries/components/entries.tsx
+++ src/features/entries/components/entries.tsx
@@ -1,7 +1,24 @@
+import { useTransactions } from "@/stores/transactionsStore"
+import Table from "./table"
+import { useEffect } from "react"
+import getTransactions from "@/lib/getTransactions"
+
 export default function Entries() {
+    const transactionsNeedRefetch = useTransactions((state) => state.needRefetch)
+    const loadTransactions = useTransactions((state) => state.loadTransactions)
+    const setTransactionsNeedRefetch = useTransactions((state) => state.setNeedRefetch)
+
+    useEffect(() => {
+        const get = async () => {
+            await getTransactions(loadTransactions, setTransactionsNeedRefetch)
+        }
+        if (transactionsNeedRefetch) {
+            get()
+        }
+    }, [])
     return (
         <>
-            <header>Entries page</header>
+            <Table/>
         </>
     )
 }
