6f862e36a6f77bec27351c595f1d3b42ea355cc5
Dawit <dawitsurithpinyo@gmail.com>
2026-05-27 19:24:56 +0700

    feat(entries): add settings on top of table for sorting based on field and whether in ascending order

--- src/features/entries/components/entries.tsx
+++ src/features/entries/components/entries.tsx
@@ -1,12 +1,19 @@
 import { useTransactions } from "@/stores/transactionsStore"
-import Table from "./table"
-import { useEffect } from "react"
 import getTransactions from "@/lib/getTransactions"
+import { useTransactionsHandling } from "@/features/entries/stores/transactionsHandling"
+import type { TransactionsSortKeys } from "@/types/transactions"
+import Table from "./table"
+
+import { useEffect } from "react"
 
 export default function Entries() {
     const transactionsNeedRefetch = useTransactions((state) => state.needRefetch)
     const loadTransactions = useTransactions((state) => state.loadTransactions)
     const setTransactionsNeedRefetch = useTransactions((state) => state.setNeedRefetch)
+
+    // Will move all these sorting/selecting stuff to their own component later
+    const setSortKey = useTransactionsHandling((state) => state.setSortKey)
+    const setSortAsc = useTransactionsHandling((state) => state.setSortAsc)
 
     useEffect(() => {
         const get = async () => {
@@ -16,8 +23,38 @@ export default function Entries() {
             get()
         }
     }, [])
+
+    function handleSubmitSortKey(key: TransactionsSortKeys): void {
+        if (!['transactionName', 'accountID', 'value', 'date', 'memo'].includes(key.toString())) {
+            console.error("Input for transactions sorting key should be one of the following: 'transactionName', 'accountID', 'value', 'date', 'memo'")
+            return
+        }
+        setSortKey(key)
+    }
+    function handleSubmitSortAsc(asc: string): void {
+        let ascLow = asc.toLowerCase()
+        if (ascLow != 'true' && ascLow != 'false') {
+            console.error("Input for setting whether to sort transactions ascending should be 'true' or 'false' only.")
+            return
+        }
+        setSortAsc(ascLow === 'true')
+    }
+
     return (
         <>
+            <label htmlFor="sortKey">Sort by:</label>
+            <select id="sortKey" defaultValue="date" onChange={e => handleSubmitSortKey(e.target.value as TransactionsSortKeys)}>
+                <option value="transactionName">transaction name</option>
+                <option value="accountID">account ID</option>
+                <option value="value">value</option>
+                <option value="date">date</option>
+                <option value="memo">memo</option>
+            </select>
+            <label htmlFor="sortAsc">Sort ascending</label>
+            <select id="sortAsc" defaultValue="true" onChange={e => handleSubmitSortAsc(e.target.value)}>
+                <option value="true">true</option>
+                <option value="false">false</option>
+            </select>
             <Table/>
         </>
     )
